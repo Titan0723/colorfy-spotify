ff23a60f50a7c30e2cb0cafbf9ff3eff5fc4d31f
Mary-Snow <snow.mary.r@gmail.com>
2017-03-31 14:38:39 -0700

    created container component in react

---
+++ client/components/container.jsx
@@ -0,0 +1,16 @@
+import React,{PropTypes} from 'react';
+
+class container extends Component{
+  constructor(props)
+  super(props)
+
+  render(){
+    return(
+      <div>
+        <h2>WTF GUYS?!?</h2>
+      </div>
+    )
+  }
+}
+
+export default container;
